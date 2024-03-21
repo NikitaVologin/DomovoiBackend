d5b7e380a10aa5dd69e7bdfc62e2c7bb578d30cd
xefheh <114147391+xefheh@users.noreply.github.com>
2024-03-21 13:12:44 +0500

    Delete CliTest directory

--- CliTest/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using DomovoiBackend.Domain.Factories.CommercialBuildingsFactories.Infos;
-using DomovoiBackend.Domain.Factories.RealityFactories.Factories;
-
-var RealityFactory = new RealityFactory();
-
-var warehouseInfo = new WarehouseBuildingInfo(15, "w", 5, true, "s", true, true, null, true);
-
-Console.WriteLine(RealityFactory.GenerateReality(warehouseInfo).Id);
