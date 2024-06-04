79106ccf0412a68df59e057aab0eecfb68ea24f2
NikitaVologin <kotgot47@gmail.com>
2024-06-05 01:27:01 +0500

    ЖВ

--- DomovoiBackend.API/Hubs/ChatHub.cs
+++ DomovoiBackend.API/Hubs/ChatHub.cs
@@ -16,7 +16,7 @@ namespace DomovoiBackend.API.Hubs
             _chatService = chatService;
 
         [Authorize]
-        public async Task Send(string message, string idReceiver, CancellationToken token)
+        public async Task Send(string message, string idReceiver)
         {
             var idSender = Context.User!.FindFirst("CounterAgentId")!.Value;
             if (Clients.User(idReceiver) == null)
@@ -30,38 +30,38 @@ namespace DomovoiBackend.API.Hubs
                 IdReceiver = Guid.Parse(idReceiver),
                 Text = message,
             };
-            var messageId = await _chatService.AddMessageAsync(request, token);
+            var messageId = await _chatService.AddMessageAsync(request, CancellationToken.None);
             await Clients.User(idReceiver).SendAsync("Receive", message, idSender, messageId, MessageStatus.Recieve);
             await Clients.Caller.SendAsync("NotifySendMethod", messageId, MessageStatus.Recieve);
         }
 
         [Authorize]
-        public async Task NotifyUserStatus(string idUser, string status, CancellationToken token)
+        public async Task NotifyUserStatus(string idUser, string status)
         {
-            var ids = await this._chatService.GetDialogsAsync(Guid.Parse(idUser), token);
+            var ids = await this._chatService.GetDialogsAsync(Guid.Parse(idUser), CancellationToken.None);
             var idsList = (IReadOnlyList<string>)ids;
             await Clients.Users(idsList).SendAsync("NotifyUserStatus", idUser, status);
         }
 
         [Authorize]
-        public async Task NotifyReadDialog(string idCompanion, CancellationToken token)
+        public async Task NotifyReadDialog(string idCompanion)
         {
             var idUser = Context.User!.FindFirst("CounterAgentId")!.Value;
             await Clients.User(idCompanion.ToString()).SendAsync("NotifyReadDialog", idUser);
         }
 
         [Authorize]
-        public async Task NotifyRemoveMessage(string idMessage, CancellationToken token)
+        public async Task NotifyRemoveMessage(string idMessage)
         {
             var idUser = Context.User!.FindFirst("CounterAgentId")!.Value;
-            await this._chatService.RemoveMessageAsync(Guid.Parse(idMessage), token);
-            var message = await this._chatService.GetMessageByIdAsync(Guid.Parse(idMessage), token);
+            await this._chatService.RemoveMessageAsync(Guid.Parse(idMessage), CancellationToken.None);
+            var message = await this._chatService.GetMessageByIdAsync(Guid.Parse(idMessage), CancellationToken.None);
             var idCompanion = idUser == message.IdReceiver.ToString() ? message.IdSender : message.IdSender;
             await Clients.User(idCompanion.ToString()).SendAsync("NotifyMessageStatus", idMessage, "", MessageStatus.Remove);
         }
 
         [Authorize]
-        public async Task NotifyChangeMessage(string idMessage, string newText, CancellationToken token)
+        public async Task NotifyChangeMessage(string idMessage, string newText)
         {
             var idUser = Context.User!.FindFirst("CounterAgentId")!.Value;
             var request = new UpdateMessageRequest
@@ -69,8 +69,8 @@ namespace DomovoiBackend.API.Hubs
                 Id = Guid.Parse(idMessage),
                 Text = newText,
             };
-            await this._chatService.UpdateMessageAsync(request, token);
-            var message = await this._chatService.GetMessageByIdAsync(Guid.Parse(idMessage), token);
+            await this._chatService.UpdateMessageAsync(request, CancellationToken.None);
+            var message = await this._chatService.GetMessageByIdAsync(Guid.Parse(idMessage), CancellationToken.None);
             var idCompanion = idUser == message.IdReceiver.ToString() ? message.IdSender : message.IdSender;
             await Clients.User(idCompanion.ToString()).SendAsync("NotifyMessageStatus", idMessage, newText, MessageStatus.Change);
         }
