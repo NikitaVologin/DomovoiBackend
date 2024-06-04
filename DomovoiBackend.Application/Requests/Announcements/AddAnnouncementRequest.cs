3cde49714c463de3bcfd5d3fc821f834f5a75b8e
xefheh <aserovik@gmail.com>
2024-06-05 02:00:49 +0500

    Подчистка ненужностей

--- DomovoiBackend.Application/Requests/Announcements/AddAnnouncementRequest.cs
+++ DomovoiBackend.Application/Requests/Announcements/AddAnnouncementRequest.cs
@@ -1,49 +1,7 @@
 using DomovoiBackend.Application.Information.Deals;
 using DomovoiBackend.Application.Information.Realities;
-using Microsoft.AspNetCore.Http;
 
 namespace DomovoiBackend.Application.Requests.Announcements;
-
-/*
- * {
-         "description": "string",
-         "connectionType": "string",
-         "dealInfo": {
-           "sellConditionInfo": {
-             "price": 0,
-             "type": "string"
-           },
-           "sellFeaturesInfo": {
-             "yearInOwn": 0,
-             "ownersCount": 0,
-             "prescribersCount": 0,
-             "haveChildOwners": true,
-             "haveChildPrescribers": true
-           }
-         },
-         "realityInfo": {
-           "area": 0,
-           "type": "string",
-           "floorsCount": 0,
-           "entry": true,
-           "address": "string",
-           "isUse": true,
-           "isAccess": true,
-           "building": {
-             "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-             "class": "string",
-             "buildingYear": 0,
-             "centerName": "string",
-             "haveParking": true,
-             "isEquipment": true
-           },
-           "name": "string",
-           "roomsCount": 0
-         },
-         "counterAgentId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
-       }
- */
-
 
 /// <summary>
 /// Запрос создания объявления.
@@ -74,6 +32,4 @@ public class AddAnnouncementRequest
     /// Id Контр-агента.
     /// </summary>
     public Guid CounterAgentId { get; set; }
-    
-    public IFormFile[] Pictures { get; set; }
 }
